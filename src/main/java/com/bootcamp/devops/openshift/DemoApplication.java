ee843d276d12f25b3f58c634dd6ed754ba9aece7
a.saavedra <a.saavedra@indizen.com>
2023-02-11 20:34:09 +0100

    first version

---
+++ src/main/java/com/bootcamp/devops/openshift/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.devops.openshift;
+
+
+import java.util.Arrays;
+
+public class DemoApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
---
+++ src/test/java/com/bootcamp/devops/openshift/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bootcamp.devops.openshift;
+
+import org.junit.Test;
+
+class DemoApplicationTests {
+
+	@Test
+	void Test() {
+	}
+
+}
